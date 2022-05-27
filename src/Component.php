28fe2e4116637017fecd0f081982230506667bbd
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-26 19:33:46 -0700

    initial commit

---
+++ src/Component.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Backdrop Fonts
+ * 
+ * @package   Backdrop Fonts
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2022. Benjamin Lu
+ * @link      https://github.com/benlumia007/backdrop-fonts
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Benlumia007\Backdrop\Fonts;
+use Benlumia007\Backdrop\Contracts\Bootable;
+
+class Component implements Bootable {
+	/**
+	 * Loads enqueue();
+	 * 
+	 * THe enqueue(); is used to define any scripts and styles that's going to be used part of a theme.
+	 * 
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function enqueue() {
+		/**
+		 * This will load Fonts as part of the theme. Fira Sans, Merriweather, and Tangerine. For more information
+		 * regarding this feature, please go to the following url. https://google-webfonts-helper.herokuapp.com/fonts
+		 */
+		array_map( function( $file ) {
+			wp_enqueue_style( "backdrop-$file", get_parent_theme_file_uri( "/vendor/benlumia007/backdrop-fonts/fonts/$file.css" ) );
+		}, [
+			'fira-sans',
+			'merriweather',
+			'tangerine'
+		] );
+	}
+}
---
+++ src/Provider.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Backdrop Fonts
+ * 
+ * @package   Backdrop Fonts
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2022. Benjamin Lu
+ * @link      https://github.com/benlumia007/backdrop-fonts
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Benlumia007\Backdrop\Fonts;
+use Benlumia007\Backdrop\Support\ServiceProvider;
+
+class Provider extends ServiceProvider {
+	/**
+	 * Binds the implementation of the attributes contract to the container.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function register() {
+
+		$this->app->bind( Component::class );
+
+		$this->app->alias( Component::class, 'backdrop/fonts' );
+    }
+    
+    public function boot() {
+        $this->app->resolve( 'backdrop/fonts' )->boot();
+    }
+}
