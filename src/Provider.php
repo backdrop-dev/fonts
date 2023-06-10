5fee4f94db8f3aec9bd6521a115a5494ebd667ab
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-10 15:06:51 -0700

    updated

--- src/Provider.php
+++ src/Provider.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * Fonts provider.
- * 
+ * Backdrop
+ *
  * @package   Backdrop
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2019-2023. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/backdrop-fonts
+ * @link      https://github.com/benlumia007/backdrop-theme
  */
 
 namespace Backdrop\Fonts;
 use Backdrop\Core\ServiceProvider;
 
+/**
+ * Theme provider.
+ *
+ * @since  1.0.0
+ * @access public
+ */
 class Provider extends ServiceProvider {
+
 	/**
-	 * Binds the implementation of the attributes contract to the container.
+	 * Bootstrap action/filter hook calls.
 	 *
 	 * @since  1.0.0
-	 * @access public
+	 * @access protected
 	 * @return void
 	 */
-	public function register() {
-
-		$this->app->bind( Component::class );
-
-		$this->app->alias( Component::class, 'backdrop/fonts' );
-    }
-    
-    public function boot() {
-        $this->app->resolve( 'backdrop/fonts' )->boot();
-    }
+	public function boot() {
+		require_once 'bootstrap-app.php';
+	}
 }
